a8efcebcd2633b274f52d4602f045cec69417a3a
jas112 <jas.sandmaster@gmail.com>
2022-07-29 01:45:13 -0700

    CardDeck modified....error handling.

--- src/components/cardDeck/CardDeck.js
+++ src/components/cardDeck/CardDeck.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import Card from '../card/Card';
 import './CardDeck.css';
 
+const API_BASE_URL = 'https://deckofcardsapi.com/api/deck/';
+
 class CardDeck extends Component {
     constructor(props){
         super(props);
@@ -11,6 +13,7 @@ class CardDeck extends Component {
             isSuccess: false,
             deck_id: '',
             remaining: 0,
+            cardsDrawn: 0,
             shuffled: false,
             outOfCards: true
         };
@@ -18,7 +21,7 @@ class CardDeck extends Component {
     }
 
     async componentDidMount(){
-        let res = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/');
+        let res = await axios.get(`${API_BASE_URL}new/shuffle/`);
         let currentDeck = res.data;
         console.log(`currentDeck Data: ${JSON.stringify(currentDeck)}`);
         this.setState({isSuccess: currentDeck.success, deck_id: currentDeck.deck_id, remaining: currentDeck.remaining, shuffled: currentDeck.shuffled, outOfCards: false});
@@ -35,16 +38,35 @@ class CardDeck extends Component {
 
     async drawCard(){
 
-        if (this.state.remaining === 0) {
-            this.setState({outOfCards: true});
-        }else{
-            let deckUrl = `https://deckofcardsapi.com/api/deck/${this.state.deck_id}/draw/`;
+        let deckId = this.state.deck_id;
+        let deckUrl = `${API_BASE_URL}${deckId}/draw/`;
+
+        try {
+            
             let res = await axios.get(deckUrl);
             let refreshedDeck = res.data;
+
             console.log(`refreshedDeck Data: ${JSON.stringify(refreshedDeck)}`);
-            let remainingDelta = this.state.remaining - 1;
-            let cardsDelta = [...this.state.cards, ...refreshedDeck.cards]
-            this.setState({isSuccess: refreshedDeck.success, cards: cardsDelta, remaining: refreshedDeck.remaining});
+
+            if (refreshedDeck.success) {
+                let remainingDelta = this.state.remaining - 1;
+                let cardsDrawnDelta = this.state.cardsDrawn + 1;
+                let cardsDelta = [...this.state.cards, ...refreshedDeck.cards];
+                this.setState({isSuccess: refreshedDeck.success, cards: cardsDelta, remaining: refreshedDeck.remaining, cardsDrawn: cardsDrawnDelta});
+            } else {
+                this.setState({outOfCards: true});
+                throw new Error('there are no cards to be drawn...')
+            }
+
+            // if((this.state.remaining === 0) && (this.state.cardsDrawn === 52)){
+            //     this.setState({outOfCards: true});
+            // }
+
+        } catch (error) {
+
+            // alert(error);
+            console.log(error);
+
         }
 
     }
@@ -53,10 +75,13 @@ class CardDeck extends Component {
 
     let cardDeck = this.generateCurrentDeck();
 
+    let deckStatus = `${this.state.cardsDrawn} drawn | ${this.state.remaining} remaining`;
+
     return (
       <div className='CardDeck'>
         <div className='CardDeck-Console'>
-            <button onClick={this.drawCard}>Draw A New Card!!!</button>
+            <div>{deckStatus}</div>
+            <button onClick={this.drawCard} disabled={this.state.outOfCards}>{this.state.outOfCards ? 'Out of Cards!!!' : 'Draw A New Card!!!'}</button>
         </div>
         <div className='CardDeck-Display'>
             {cardDeck}
