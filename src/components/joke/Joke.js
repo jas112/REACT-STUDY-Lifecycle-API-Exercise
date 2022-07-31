718bfe4fa03b1bdf5feaae185e00e6535b8f89d9
jas112 <jas.sandmaster@gmail.com>
2022-07-30 20:35:59 -0700

    added upvote/downvote and addJokes functions...added styles.

--- src/components/joke/Joke.js
+++ src/components/joke/Joke.js
@@ -2,19 +2,40 @@ import React, { Component } from 'react';
 import './Joke.css';
 
 class Joke extends Component {
+    constructor(props){
+        super(props);
+        this.handleUpVote = this.handleUpVote.bind(this);
+        this.handleDownVote = this.handleDownVote.bind(this);
+        this.handleSetCurrentJoke = this.handleSetCurrentJoke.bind(this);
+    }
+
+    handleUpVote(){
+        this.props.generateUpVote(this.props.idx);
+    }
+
+    handleDownVote(){
+        this.props.generateDownVote(this.props.idx);
+    }
+
+    handleSetCurrentJoke(){
+        this.props.setCurrentJoke(this.props.jokeObject);
+    }
+
   render() {
+
     return (
-      <div className='Joke'>
+      <div className={this.props.isCurrent ? 'Joke Joke-isCurrent' : 'Joke'} onClick={this.handleSetCurrentJoke}>
         <div className='Joke-Console'>
-            <div className='Joke-Console-Btn Joke-UpVote'>
-                <button className='Joke-Vote-Btn Joke-Vote-Btn-Up'></button>
+            <div className='Joke-Console-Btn Joke-UpVote' onClick={this.handleUpVote}>
+                <div className='Joke-Vote-Btn Joke-Vote-Btn-Up'>&uarr;</div>
             </div>
-            <div className='Joke-Content'>{this.props.joke}</div>
-            <div className='Joke-Console-Btn Joke-DownVote'>
-                <button className='Joke-Vote-Btn Joke-Vote-Btn-Down'></button>
+            <div className='Joke-Vote-Total'>{this.props.voteTotal}</div>
+            <div className='Joke-Console-Btn Joke-DownVote' onClick={this.handleDownVote}>
+                <div className='Joke-Vote-Btn Joke-Vote-Btn-Down'>&darr;</div>
             </div>
         </div>
-        
+        <div className='Joke-Content'>{this.props.joke}</div>
+        <div className='Joke-Vote-Expression'></div>
       </div>
     )
   }
--- src/components/jokeList/JokeList.js
+++ src/components/jokeList/JokeList.js
@@ -12,60 +12,119 @@ class JokeList extends Component {
     constructor(props){
         super(props);
         this.state = {
-            jokes: []
+            jokes: [],
+            currJoke: {},
         };
-
+        this.addMoreJokes = this.addMoreJokes.bind(this);
         this.generateUpVote = this.generateUpVote.bind(this);
         this.generateDownVote = this.generateDownVote.bind(this);
+        this.setCurrentJoke = this.setCurrentJoke.bind(this);
     }
 
     async componentDidMount(){
 
-        let initialJokes = [];
+        let initialJokes = await this.getJokes();
 
-        while (initialJokes.length < 10) {
+        // console.log(`joke => ${JSON.stringify(initialJokes)}`);
+        console.log(`initialJokes.length => ${initialJokes.length}`);
+        this.setState({jokes: initialJokes})
+    }
+
+    async getJokes(){
+        let jokeBatch = [];
+
+        while (jokeBatch.length < 10) {
             
             let joke = {};
             
             let res = await axios.get(`${API_BASE_URL}`,{headers: {Accept: 'application/json'}});
-            console.log(`res.data => ${JSON.stringify(res.data)}`);
+            // console.log(`res.data => ${JSON.stringify(res.data)}`);
 
             joke['id'] = res.data.id;
             joke['joke'] = res.data.joke;
             joke['upVotes'] = 0;
             joke['downVotes'] = 0;
+            joke['voteTotal'] = 0;
+            joke['isCurrent'] = false;
 
-            console.log(`joke => ${JSON.stringify(joke)}`);
+            // console.log(`joke => ${JSON.stringify(joke)}`);
 
-            let isIncluded = initialJokes.includes(joke);
-            console.log(`isIncluded => ${isIncluded}`);
+            let isIncluded = jokeBatch.includes(joke);
+            // console.log(`isIncluded => ${isIncluded}`);
 
             if(!isIncluded){
-                initialJokes.push(joke);
+                jokeBatch.push(joke);
             }
             
         }
 
-        console.log(`joke => ${JSON.stringify(initialJokes)}`);
-        console.log(`initialJokes.length => ${initialJokes.length}`);
+        return jokeBatch;
+    }
+
+    async addMoreJokes(){
+        let additionalJokes = await this.getJokes();
+        let newJokes = [...this.state.jokes, ...additionalJokes];
+        this.setState({jokes: newJokes});
     }
 
     generateJokes(){
-        let jokes = this.state.jokes.map(j => (
-            <Joke key={j.id} joke={j.joke} upVotes={j.upVotes} downVotes={j.downVotes} idx={this.state.jokes.indexOf(j)} />
+        let jokes = this.state.jokes.sort((a,b) => Number(b.voteTotal) - Number(a.voteTotal)).map(j => (
+            <Joke key={j.id} jokeObject={j} joke={j.joke} upVotes={j.upVotes} downVotes={j.downVotes} voteTotal={j.voteTotal} idx={this.state.jokes.indexOf(j)} generateUpVote={this.generateUpVote} generateDownVote={this.generateDownVote} isCurrent={j.isCurrent} setCurrentJoke={this.setCurrentJoke} />
         ));
+
+        return jokes;
     }
 
     generateUpVote(idx){
+        console.log(`@generateUpVote idx => ${idx}`);
         let newJokes = [...this.state.jokes];
-        newJokes[idx].upVotes =+ 1;
+        console.log(`Pre @generateUpVote newJokes[${idx}] => ${JSON.stringify(newJokes[idx])}`);
+        newJokes[idx].upVotes += 1;
+        newJokes[idx].voteTotal += 1;
+        console.log(`Post @generateUpVote newJokes[${idx}] => ${JSON.stringify(newJokes[idx])}`);
         this.setState({jokes: newJokes});
     }
 
     generateDownVote(idx){
+        console.log(`@generateDownVote idx => ${idx}`);
         let newJokes = [...this.state.jokes];
-        newJokes[idx].downVotes =+ 1;
+        console.log(`Pre @generateDownVote newJokes[${idx}] => ${JSON.stringify(newJokes[idx])}`);
+        newJokes[idx].downVotes += 1;
+        newJokes[idx].voteTotal -= 1;
+        console.log(`Post @generateDownVote newJokes[${idx}] => ${JSON.stringify(newJokes[idx])}`);
         this.setState({jokes: newJokes});
+    }
+
+    setCurrentJoke(joke){
+        // console.log(`@setCurrentJoke idx => ${this.state.jokes.indexOf(joke)}`);
+
+        let newJokes = [...this.state.jokes];
+
+        let currIdx = newJokes.indexOf(this.state.currJoke);
+        let newCurrIdx = newJokes.indexOf(joke);
+
+        console.log(`currIdx: ${currIdx} | newCurrIdx: ${newCurrIdx}`);
+
+        if(currIdx >= 0){
+            newJokes[currIdx].isCurrent = false;
+        }
+
+        newJokes[newCurrIdx].isCurrent = true;
+
+        // console.log(`Pre @setCurrentJoke newJokes[${idx}] => ${JSON.stringify(newJokes[idx])}`);
+
+        // if(newJokes.includes(this.currJoke)){
+        //     newJokes[currIdx].isCurrent = false;
+        //     newJokes[newCurrIdx].isCurrent = true;
+        // }else{
+        //     newJokes[newCurrIdx].isCurrent = true;
+        // }
+        
+        
+
+        // console.log(`Post @setCurrentJoke newJokes[${idx}] => ${JSON.stringify(newJokes[idx])}`);
+
+        this.setState({currJoke: joke});
     }
 
   render() {
@@ -74,13 +133,20 @@ class JokeList extends Component {
 
     return (
       <div className='JokeList'>
-        <div className='JokeList-Console'>
-            <div className='Jokelist-Markee'></div>
-            <div className='JokeList-Control'></div>
+        <div className='JokeList-Container'>
+            <div className='JokeList-Console'>
+                <div className='JokeList-Markee'>
+                    <h1>Joke List</h1>
+                </div>
+                <div className='JokeList-Control'>
+                    <button className='JokeList-Control-Btn' onClick={this.addMoreJokes}>Add Jokes</button>
+                </div>
+            </div>
+            <div className='JokeList-Display'>
+                {currentJokes}
+            </div>
         </div>
-        <div className='JokeList-Display'>
-            {currentJokes}
-        </div>
+        
     </div>
     )
   }
